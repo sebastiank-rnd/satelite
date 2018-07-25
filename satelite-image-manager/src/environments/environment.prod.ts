ea1af9bab391e4c658635552e888687aa271c161
sebastiank-rnd <ext_sebastian.katz@rnd-hub.com>
2018-07-25 15:36:42 +0300

    Updted api gateway url

--- satelite-image-manager/src/environments/environment.prod.ts
+++ satelite-image-manager/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  gatewayUrl: 'http://localhost:3001',
+  gatewayUrl: 'http://http://ec2-54-218-170-126.us-west-2.compute.amazonaws.com:32725',
   auth0: {
     domain: 'satimisighta.auth0.com',
     clientId: 'XtI-4B1EfV_6EbKGZilSIjLTiUuy5fhR',
-    audience: 'http://localhost:3001/api',
+    audience: 'http://lhttp://ec2-54-218-170-126.us-west-2.compute.amazonaws.com:32725',
     redirect: 'http://localhost:4200/callback',
     home: 'http://localhost:4200',
     scope: 'openid profile email'
