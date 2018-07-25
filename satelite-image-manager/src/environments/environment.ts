3077cc805fa2b52508758327cb3c142fa9dd23a8
sebastiank-rnd <ext_sebastian.katz@rnd-hub.com>
2018-07-25 16:21:01 +0300

    Updated gateway url

--- satelite-image-manager/src/environments/environment.ts
+++ satelite-image-manager/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  gatewayUrl: 'http://http://ec2-54-218-170-126.us-west-2.compute.amazonaws.com:32725',
+  gatewayUrl: 'http://gw.sat.imisight.net/',
   auth0: {
     domain: 'satimisighta.auth0.com',
     clientId: 'XtI-4B1EfV_6EbKGZilSIjLTiUuy5fhR',
