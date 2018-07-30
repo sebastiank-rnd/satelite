f217402cba97dd911da879dc4efb500ff82880c0
sebastiank-rnd <ext_sebastian.katz@rnd-hub.com>
2018-07-30 18:11:05 +0300

    Fixed gateway url

--- satelite-image-manager/src/environments/environment.prod.ts
+++ satelite-image-manager/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  gatewayUrl: 'https://imisight-sat.auth0.com/api/v2/',
+  gatewayUrl: 'https://gw.sat.imisight.net/',
   auth0: {
     domain: 'satimisighta.auth0.com',
     clientId: 'KXLTbs08LtLqrbPwSgn7Ioej0aMB7tf6',
-    audience: 'https://imisight-sat.auth0.com/api/v2/',
+    audience: 'https://gw.sat.imisight.net/',
     redirect: 'http://localhost:4200/callback',
     home: 'http://localhost:4200',
     scope: 'openid profile'
